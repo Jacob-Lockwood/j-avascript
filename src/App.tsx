5eee079545282aec54eab3e6e446987ecbe491f0
Jacob <jacobulysseslockwood@gmail.com>
2024-09-15 18:04:41 -0400

    Added URL functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import J from "./j-avascript";
 
 export default function App() {
-  const [header, setHeader] = createSignal("const f =");
-  const [code, setCode] = createSignal("J`,join${' '}+${'!'}`");
-  const [footer, setFooter] = createSignal("console.log(f('Hello', 'World'))");
-  const program = () => header() + "\n" + code() + "\n" + footer();
+  const { searchParams } = new URL(window.location.href);
+  const [header, setHeader] = createSignal(
+    searchParams.get("h") ?? "const f ="
+  );
+  const [code, setCode] = createSignal(
+    searchParams.get("c") ?? "J`,join${', '}+${'!'}`"
+  );
+  const [footer, setFooter] = createSignal(
+    searchParams.get("f") ?? "console.log(f('Hello', 'World'))"
+  );
+  createEffect(() => {
+    const pageURL = window.location.origin + window.location.pathname;
+    window.history.replaceState(
+      null,
+      document.title,
+      pageURL +
+        `?h=${encodeURIComponent(header())}&c=${encodeURIComponent(code())}&f=${encodeURIComponent(footer())}`
+    );
+  });
+  const byteCount = () => new TextEncoder().encode(code()).length;
+
   const [output, setOutput] = createSignal("");
   const [error, setError] = createSignal("");
 
+  const block =
+    "bg-stone-800/80 text-white p-5 w-full h-min focus:ring-0 focus:outline-none border-solid border-2 border-stone-900 focus:border-rose-600 overflow-x-scroll whitespace-pre";
+  const btn =
+    "border-2 border-rose-500 bg-rose-700 w-max px-10 hover:bg-rose-600 hover:text-rose-100 hover:border-rose-400";
   const example = (name: string, head: string, codez: string, foot: string) => (
     <button
       class="hover:bg-rose-800 underline underline-offset-2 w-max flex-shrink-0"
@@ -21,12 +42,12 @@ export default function App() {
       {name}
     </button>
   );
-
   return (
     <main class="flex flex-col gap-2 font-mono bg-neutral-900/50 md:w-3/4 p-10 mx-auto min-h-screen text-rose-200">
       <div class="border-2 border-solid border-rose-700 px-2 py-1 bg-neutral-900/50">
         <p>Load examples:</p>
         <div class="flex overflow-x-auto gap-x-10 pb-2">
+          {example("Hello, World!", "(", 'J`,join${", "}+${"!"}`', ")()")}
           {example(
             "Average of a List",
             "const f =",
@@ -53,16 +74,16 @@ export default function App() {
         name="header"
         id="header"
         onInput={({ target }) => setHeader(target.value)}
-        class="bg-stone-800/80 text-white p-5 w-full h-min focus:ring-0 focus:outline-none border-solid border-2 border-stone-900 focus:border-rose-600"
+        class={block}
       >
         {header()}
       </textarea>
-      <label for="code">Code: ({code().length} bytes)</label>
+      <label for="code">Code: ({byteCount()} bytes)</label>
       <textarea
         name="code"
         id="code"
         onInput={({ target }) => setCode(target.value)}
-        class="bg-stone-800/80 text-white p-5 w-full focus:ring-0 focus:outline-none border-solid border-2 border-stone-900 focus:border-rose-600"
+        class={block}
       >
         {code()}
       </textarea>
@@ -71,17 +92,21 @@ export default function App() {
         name="footer"
         id="footer"
         onInput={({ target }) => setFooter(target.value)}
-        class="bg-stone-800/80 text-white p-5 w-full h-min focus:ring-0 focus:outline-none border-solid border-2 border-stone-900 focus:border-rose-600"
+        class={block}
       >
         {footer()}
       </textarea>
       <button
-        class="border-2 border-rose-500 bg-rose-700 w-max px-10 hover:bg-rose-600 hover:text-rose-100 hover:border-rose-400"
+        class={btn}
         onClick={() => {
           console.clear();
           try {
             setOutput("");
-            new Function("J", "console", program())(J, {
+            new Function(
+              "J",
+              "console",
+              header() + "\n" + code() + "\n" + footer()
+            )(J, {
               log: (...vals: unknown[]) =>
                 setOutput(output() + vals.join(" ") + "\n"),
             });
@@ -93,13 +118,37 @@ export default function App() {
         Run
       </button>
       <p>Output:</p>
-      <pre class="bg-stone-800/80 text-white p-5 w-full border-solid border-2 border-stone-900">
+      <textarea class={block} readonly>
         {output()}
-      </pre>
+      </textarea>
       <p>Error:</p>
-      <pre class="bg-stone-800/80 text-white p-5 w-full border-solid border-2 border-stone-900">
+      <textarea class={block} readonly>
         {error()}
-      </pre>
+      </textarea>
+
+      <p class="text-lg font-semibold mt-16 mb-4">Share this program</p>
+      <p>URL:</p>
+      <textarea class={block} readonly rows={1}>
+        {location.href}
+      </textarea>
+      <p>Markdown (CMC):</p>
+      <textarea class={block} readonly rows={1}>
+        {`J-avaScript, ${byteCount()} bytes - \`\`${code()}\`\` [try it](${location.href})`}
+      </textarea>
+      <p>Markdown (CGCC):</p>
+      <textarea class={block} readonly rows={8}>
+        {`# [J-avaScript], ${byteCount()} bytes
+
+${"```"}javascript
+${code()}
+${"```"}
+
+[Try it!](${location.href})
+
+
+  [J-avaScript]: https://github.com/Jacob-Lockwood/j-avascript
+`}
+      </textarea>
     </main>
   );
 }
